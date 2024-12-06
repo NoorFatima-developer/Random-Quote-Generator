f452e26da25b7552446d4dc9e291a5740867468d
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-06 10:05:28 +0500

    code is added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const api_url = "https://api.quotable.io/random";
+const api_url = "https://dummyjson.com/quotes/random";
+
+async function randoquotes(url){
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data); 
+}
+
+randoquotes(api_url);
