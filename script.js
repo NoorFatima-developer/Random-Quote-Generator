b5f5364d3e7853a324546098dfb559d14881e6e7
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-06 10:53:13 +0500

    code is added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const api_url = "https://dummyjson.com/quotes/random";
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const newquote = document.getElementById("newquote");
+const tweet = document.getElementById("tweet");
 
 async function randomquotes(url){
     const response = await fetch(url)
@@ -13,6 +14,16 @@ async function randomquotes(url){
 
 randomquotes(api_url);
 
+function tweeter(){
+    window.open("https://twitter.com/intent/tweet?text=Hello%20World");
+}
+
+// must use function keyword because we are passing argument so osko btana pryga k ye func hain..
+// aghr ni argument pass krna chahty tu dont use func but osklye fer directly fetch k andr url pass krna pryga ta k isko randomquotes(api_url) hum essy call krain bs
+// randomquotes(), then hi hum eventlistener m without func likh skty hain..
 newquote.addEventListener("click", function(){
     randomquotes(api_url)
 })
+
+// use this way without event listener...
+tweet.addEventListener("click", tweeter);
